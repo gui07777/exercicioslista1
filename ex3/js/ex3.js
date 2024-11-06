9b320d0c0a82260e1a6cef567e9160197efc64a9
gui07777 <guilherme2006.gmc@gmail.com>
2024-11-05 23:51:30 -0300

    Add files via upload

---
+++ ex3/js/ex3.js
@@ -0,0 +1,5 @@
+var n1 = parseFloat(prompt("Digite o primeiro número:"));
+var n2 = parseFloat(prompt("Digite o segundo número:"));
+var total = n1 + n2;
+
+alert("O total é:", + total); 
---
+++ ex4/js/ex4.js
@@ -0,0 +1,8 @@
+var n1 = parseFloat(prompt("Primeira nota:"));
+var n2 = parseFloat(prompt("Segunda nota:"));
+var n3 = parseFloat(prompt("Terceira nota:"));
+var n4 = parseFloat(prompt("Quarta nota:"));
+var media = ( n1 + n2 + n3 + n4 ) / 4;
+{
+alert("Média final:", media);
+}
